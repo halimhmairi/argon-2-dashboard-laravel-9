8311be02feccdd6c46ebe522ac2dc62eb010215d
Halim Hmairi <hmairihalim@gmail.com>
2022-06-12 12:47:57 +0100

    fixed nav bar dashboard

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,7 +19,7 @@
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
-<body>
+<body class="g-sidenav-show   bg-gray-100">
     <div id="app">
         @guest
         <div class="container position-sticky z-index-sticky top-0">
@@ -88,15 +88,15 @@
                         <aside class="sidenav bg-white navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-4 " id="sidenav-main">
                           <div class="sidenav-header">
                             <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
-                            <a class="navbar-brand m-0" href=" https://demos.creative-tim.com/argon-dashboard/pages/dashboard.html " target="_blank">
-                              <img src="../assets/img/logo-ct-dark.png" class="navbar-brand-img h-100" alt="main_logo">
-                              <span class="ms-1 font-weight-bold">Argon Dashboard 2</span>
+                            <a class="navbar-brand m-0"  href="{{ url('/home') }}" target="_blank">
+                              <img src="{{ asset('img/logo-ct-dark.png') }}" class="navbar-brand-img h-100" alt="main_logo">
+                              <span class="ms-1 font-weight-bold">  {{ config('app.name', 'Laravel') }}</span>
                             </a>
                           </div>
                           <hr class="horizontal dark mt-0">
                           <div class="collapse navbar-collapse  w-auto " id="sidenav-collapse-main">
                             <ul class="navbar-nav">
-                                @can('is_admin')
+                            @can('is_user')
                               <li class="nav-item">
                                 <a class="nav-link active" href="{{ route('category') }}">
                                   <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
@@ -106,92 +106,35 @@
                                 </a>
                               </li> 
                           @endif
-                              <li class="nav-item">
-                                <a class="nav-link " href="../pages/tables.html">
-                                  <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
-                                    <i class="ni ni-calendar-grid-58 text-warning text-sm opacity-10"></i>
-                                  </div>
-                                  <span class="nav-link-text ms-1">Tables</span>
-                                </a>
-                              </li>
-                              <li class="nav-item">
-                                <a class="nav-link " href="../pages/billing.html">
-                                  <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
-                                    <i class="ni ni-credit-card text-success text-sm opacity-10"></i>
-                                  </div>
-                                  <span class="nav-link-text ms-1">Billing</span>
-                                </a>
-                              </li>
-                              <li class="nav-item">
-                                <a class="nav-link " href="../pages/virtual-reality.html">
-                                  <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
-                                    <i class="ni ni-app text-info text-sm opacity-10"></i>
-                                  </div>
-                                  <span class="nav-link-text ms-1">Virtual Reality</span>
-                                </a>
-                              </li>
-                              <li class="nav-item">
-                                <a class="nav-link " href="../pages/rtl.html">
-                                  <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
-                                    <i class="ni ni-world-2 text-danger text-sm opacity-10"></i>
-                                  </div>
-                                  <span class="nav-link-text ms-1">RTL</span>
-                                </a>
-                              </li>
-                              <li class="nav-item mt-3">
-                                <h6 class="ps-4 ms-2 text-uppercase text-xs font-weight-bolder opacity-6">Account pages</h6>
-                              </li>
-                              <li class="nav-item">
-                                <a class="nav-link " href="../pages/profile.html">
-                                  <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
-                                    <i class="ni ni-single-02 text-dark text-sm opacity-10"></i>
-                                  </div>
-                                  <span class="nav-link-text ms-1">Profile</span>
-                                </a>
-                              </li>
-                              <li class="nav-item">
-                                <a class="nav-link " href="../pages/sign-in.html">
-                                  <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
-                                    <i class="ni ni-single-copy-04 text-warning text-sm opacity-10"></i>
-                                  </div>
-                                  <span class="nav-link-text ms-1">Sign In</span>
-                                </a>
-                              </li>
-                              <li class="nav-item">
-                                <a class="nav-link " href="../pages/sign-up.html">
-                                  <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
-                                    <i class="ni ni-collection text-info text-sm opacity-10"></i>
-                                  </div>
-                                  <span class="nav-link-text ms-1">Sign Up</span>
-                                </a>
-                              </li>
-                              <li>
-<a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-
-                              </li>
-                              
+                          @can('is_admin')
+                               <li class="nav-item">
+                                 <a class="nav-link" href="{{ route('role') }}">
+                                   <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
+                                     <i class="ni ni-calendar-grid-58 text-warning text-sm opacity-10"></i>
+                                   </div>
+                                   <span class="nav-link-text ms-1">{{ __('Role Management') }}</span>
+                                 </a>
+                               </li> 
+                               <li class="nav-item">
+                                 <a class="nav-link" href="{{ route('user') }}">
+                                   <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
+                                     <i class="ni ni-credit-card text-success text-sm opacity-10"></i>
+                                   </div>
+                                   <span class="nav-link-text ms-1">{{ __('User Management') }}</span>
+                                 </a>
+                               </li> 
+                            @endcan    
                             </ul>
                           </div>
-                          <div class="sidenav-footer mx-3 ">
-                            <div class="card card-plain shadow-none" id="sidenavCard">
-                              <img class="w-50 mx-auto" src="../assets/img/illustrations/icon-documentation.svg" alt="sidebar_illustration">
-                              <div class="card-body text-center p-3 w-100 pt-0">
-                                <div class="docs-info">
-                                  <h6 class="mb-0">Need help?</h6>
-                                  <p class="text-xs font-weight-bold mb-0">Please check our docs</p>
-                                </div>
-                              </div>
-                            </div>
-                            <a href="https://www.creative-tim.com/learning-lab/bootstrap/license/argon-dashboard" target="_blank" class="btn btn-dark btn-sm w-100 mb-3">Documentation</a>
-                            <a class="btn btn-primary btn-sm mb-0 w-100" href="https://www.creative-tim.com/product/argon-dashboard-pro?ref=sidebarfree" type="button">Upgrade to pro</a>
+                          <div class="sidenav-footer mx-3 "> 
+                            <a href="{{ route('logout') }}"
+                            onclick="event.preventDefault();
+                                          document.getElementById('logout-form').submit();"  class="btn btn-dark btn-sm w-100 mb-3">
+                                          {{ __('Logout') }}
+                                        </a>
+                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                          @csrf
+                                      </form>
                           </div>
                         </aside>
 
@@ -310,50 +253,11 @@
                     </div>
                     </div>
                 </nav>
-                <!-- End Navbar -->
-
-                                    <!-- Right Side Of Navbar -->
-                                <ul class="navbar-nav ms-auto">
-                                        <li class="nav-item dropdown">
-                                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                                {{ Auth::user()->name }}
-                                            </a>
-                                            
-                                            <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                            @can('is_user')
-                                                <a class="dropdown-item" href="{{ route('category') }}">
-                                                    {{ __('Category Management') }}
-                                                </a>
-                                            @endif
-
-                                            @can('is_admin')
-                                                <a class="dropdown-item" href="{{ route('role') }}">
-                                                    {{ __('Role Management') }}
-                                                </a>
-
-                                            <a class="dropdown-item" href="{{ route('user') }}">
-                                                    {{ __('User Management') }}
-                                                </a>
-                                            @endif
-                                            <a class="dropdown-item" href="{{ route('profile/edit') }}">
-                                                    {{ __('Settings') }}
-                                                </a>
-
-                                                <a class="dropdown-item" href="{{ route('logout') }}"
-                                                onclick="event.preventDefault();
-                                                                document.getElementById('logout-form').submit();">
-                                                    {{ __('Logout') }}
-                                                </a>
-
-                                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                                    @csrf
-                                                </form>
-                                            </div>
-                                        </li>
-                                        @include('sweetalert::alert')
-                                    </ul> 
+                <!-- End Navbar --> 
+                   @include('sweetalert::alert')
+                                   
                                     @endguest 
-                    <main class="py-4">
+                    <main class="container-fluid py-4">
                         @yield('content')
                     </main>
                 </div>
